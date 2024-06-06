c586485b976efbdf3a856a3f91d9fc3d2ec530cc
takacs <davidetkcs@gmail.com>
2024-06-06 19:24:40 +0200

    add table for navigation

--- tui/view.go
+++ tui/view.go
@@ -3,21 +3,61 @@ package tui
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
-	db "github.com/takacs/donkey/db"
+	lipgloss "github.com/charmbracelet/lipgloss"
 )
 
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
 type Model struct {
-	cards []db.Card
+	table table.Model
 }
 
 func InitProject(path string) (tea.Model, tea.Cmd) {
-	m := StatsModel{name: "stats"}
+	m := Model{table: createTable()}
 	return m, func() tea.Msg { return "hi" }
 }
 
 func (m Model) Init() tea.Cmd {
 	return nil
+}
+
+func createTable() table.Model {
+	columns := []table.Column{
+		{Title: "Rank", Width: 4},
+		{Title: "City", Width: 10},
+		{Title: "Country", Width: 10},
+		{Title: "Population", Width: 10},
+	}
+
+	rows := []table.Row{
+		{"1", "Tokyo", "Japan", "37,274,000"},
+		{"2", "Delhi", "India", "32,065,760"},
+		{"3", "Shanghai", "China", "28,516,904"},
+		{"4", "Dhaka", "Bangladesh", "22,478,116"},
+	}
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+	return t
+
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -34,5 +74,5 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	return m.cards[0].Front
+	return baseStyle.Render(m.table.View()) + "\n"
 }
