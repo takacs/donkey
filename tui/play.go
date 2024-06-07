b665bb890bc813ec4eca248f3e66478e46d09aaa
takacs <davidetkcs@gmail.com>
2024-06-07 11:33:06 +0200

    add back option to play view this probably should be abstracted out so all views will have the same basic key controls

--- tui/play.go
+++ tui/play.go
@@ -3,10 +3,40 @@ package tui
 import (
 	"fmt"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/takacs/donkey/db"
 )
 
+type keyMap struct {
+	Back key.Binding
+}
+
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Back}
+}
+
+// FullHelp returns keybindings for the expanded help view. It's part of the
+// key.Map interface.
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Back}, // first column
+	}
+}
+
+var keys = keyMap{
+	Back: key.NewBinding(
+		key.WithKeys("B", "b"),
+		key.WithHelp("b/B", "go back to main menu"),
+	),
+}
+
 type PlayModel struct {
+	keys keyMap
+	help help.Model
 	name string
 }
 
@@ -19,6 +49,12 @@ func (m PlayModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
+		case key.Matches(msg, m.keys.Back):
+			path, err := db.GetDbPath("cards")
+			if err != nil {
+				fmt.Println("error getting db path")
+			}
+			return InitProject(path)
 		default:
 			fmt.Printf("default press quit %v \n", msg)
 			return m, tea.Quit
@@ -28,5 +64,15 @@ func (m PlayModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m PlayModel) View() string {
-	return m.name
+	helpView := m.help.View(m.keys)
+
+	return m.name + "\n" + helpView
+}
+
+func newPlayModel() PlayModel {
+	return PlayModel{
+		name: "play",
+		help: help.New(),
+		keys: keys,
+	}
 }
